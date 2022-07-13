9376fdd9dd7fdc62b88f5fee94ed49b145a3266b
it_st it <overult01@gmail.com>
2022-07-13 23:55:29 +0900

    week3_day3: 220713_가장 짧은 문자거리

---
+++ week1/jungmin/inflearn/_1string/_0110/Main_teacher.java
@@ -0,0 +1,42 @@
+package week1.jungmin.inflearn._1string._0110;
+import java.util.Scanner;
+
+//가장 짧은 문자거리
+//week3_day3: 220713
+//강의 
+
+public class Main_teacher {
+	public int[] solution(String s, char t){
+		int[] answer=new int[s.length()];
+		int p=1000;
+		for(int i=0; i<s.length(); i++){
+			if(s.charAt(i)==t){
+				p=0;
+				answer[i]=p;
+			}
+			else{
+				p++;
+				answer[i]=p;
+			}
+		}
+		p=1000;
+		for(int i=s.length()-1; i>=0; i--){
+			if(s.charAt(i)==t) p=0;
+			else{
+				p++;
+				answer[i]=Math.min(answer[i], p);
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main_teacher T = new Main_teacher();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		char c=kb.next().charAt(0);
+		for(int x : T.solution(str, c)){
+			System.out.print(x+" ");
+		}
+	}
+}
