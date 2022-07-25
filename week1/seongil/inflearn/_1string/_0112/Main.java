82578b9b11340a01f2016b953747e12c96c8585f
Seongil, Yoon <dbs3346@gmail.com>
2022-07-25 13:31:43 +0900

    성일-5주차-문자열3문제

---
+++ week1/seongil/inflearn/_1string/_0112/Main.java
@@ -0,0 +1,31 @@
+package week1.seongil.inflearn._1string._0112;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 12. 암호
+ * 
+ * @author Seongil, Yoon
+ *
+ */
+class Main {	
+	public String solution(int n, String s){
+		String answer="";
+		for(int i=0; i<n; i++){
+			String tmp=s.substring(0, 7).replace('#', '1').replace('*', '0');
+			int num=Integer.parseInt(tmp, 2);
+			answer+=(char)num;
+			s=s.substring(7);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		String str=kb.next();
+		System.out.println(T.solution(n, str));
+	}
+}
